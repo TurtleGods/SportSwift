9c573221ec52b6ca6ac9dbc93f7e92b1e9f6fbd6
Turtlegods <strollist@gmail.com>
2025-12-06 21:59:45 +0800

    直接改版

---
+++ Sport/Sport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sport
+//
+//  Created by 佳緯周 on 2025/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sport/Sport/SportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportApp.swift
+//  Sport
+//
+//  Created by 佳緯周 on 2025/12/6.
+//
+
+import SwiftUI
+
+@main
+struct SportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
